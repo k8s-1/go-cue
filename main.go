317c7d9f8b3504e7e36c243fbb1f53c1c52fb2e4
pman <pman-$-432894328923@gmail.com>
2024-10-19 10:34:27 +0200

    chore: update file(s): main.go

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 
 	"cuelang.org/go/cue/cuecontext"
-	"cuelang.org/go/cue/load"
+	// "cuelang.org/go/cue/load"
 )
 
 type Person struct {
@@ -14,17 +14,17 @@ type Person struct {
 }
 
 func main() {
-  ctx := cuecontext.New()
-	insts := load.Instances([]string{"."}, nil) // build max 1 package in current dir
-	v := ctx.BuildInstance(insts[0])
-	fmt.Printf("%v\n", v)
-	// ctx := cuecontext.New()
-	//
-	// person := Person{
-	// 	Name: "Charlie Cartwright",
-	// 	Age:  999,
-	// }
-	//
-	// personAsCUE := ctx.Encode(person)
+	//  ctx := cuecontext.New()
+	// insts := load.Instances([]string{"."}, nil) // build max 1 package in current dir
+	// v := ctx.BuildInstance(insts[0])
+	// fmt.Printf("%v\n", v)
+	ctx := cuecontext.New()
 
+	person := Person{
+		Name: "Charlie Cartwright",
+		Age:  999,
+	}
+
+	personAsCUE := ctx.Encode(person)
+	fmt.Printf("%personAsCUE\n", personAsCUE)
 }
