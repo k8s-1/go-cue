1ada481b7d0c6575aef72c629252cca28de3f885
pman <pman-$-432894328923@gmail.com>
2024-10-19 12:53:37 +0200

    chore: update file(s): dynamic.go

--- dynamic.go
+++ dynamic.go
@@ -10,6 +10,13 @@ func dynamicstruct() {
 		Age:  999,
 	}
 
+  // Check if "Age" exists in dynamicValues and assign it
+  if age, ok := dynamicValues["Age"]; ok {
+      if ageValue, valid := age.(int); valid { // Ensure the type is correct
+          person.Age = ageValue
+      }
+  }
+
 	fmt.Printf("%v\n", person)
 
 }
